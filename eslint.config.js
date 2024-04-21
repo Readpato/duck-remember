5f96ef156bc93ae8d23b8b28d4cf7aac2ade8dc2
Patrick Raedler <code@readpato.dev>
2024-04-21 15:40:07 +0200

    chore(deps): install eslint tailwind module

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,19 @@
 import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
 
 export default await antfu({
   rules: {
     'antfu/top-level-function': 'off',
   },
-})
+}, ...compat.config({
+  extends: [
+    'plugin:tailwindcss/recommended',
+  ],
+  settings: {
+    tailwindcss: {
+      whitelist: ['app'],
+    },
+  },
+}))
