9fff9f8cdf565c4539f02c790d0d29129afa9521
Patrick Raedler <code@readpato.dev>
2024-04-21 17:57:55 +0200

    feat(TheNavbar): create component

---
+++ components/TheNavbar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const colorModeIcon = computed(() => colorMode.preference === 'dark' ? 'sun' : 'moon')
+
+const handleColorChange = () => {
+  colorMode.preference === 'dark'
+    ? colorMode.preference = 'light'
+    : colorMode.preference = 'dark'
+}
+</script>
+
+<template>
+  <header class="flex items-center justify-between p-4">
+    <h1 class="scroll-m-20 text-2xl font-semibold tracking-tight">
+      Remember
+    </h1>
+    <div class="space-x-2">
+      <CtaIcon icon="logo-github" as="anchor" to="https://www.github.com" target="_blank" />
+      <CtaIcon
+        :icon="colorModeIcon"
+        @click="handleColorChange"
+      />
+    </div>
+  </header>
+</template>
