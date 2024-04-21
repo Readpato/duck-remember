f177ad783b59c02b35f31d6b59c58728e118d083
Patrick Raedler <code@readpato.dev>
2024-04-21 17:58:06 +0200

    feat(CtaIcon): create component

---
+++ components/CtaIcon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { ButtonVariants } from '~/components/ui/button'
+
+withDefaults(defineProps<{
+  icon: string
+  variant?: ButtonVariants['variant']
+}>(), {
+  variant: 'outline',
+})
+</script>
+
+<template>
+  <SCButton size="icon" :variant="variant" class="size-8">
+    <Icon :name="`carbon:${icon}`" class="size-5" />
+  </SCButton>
+</template>
