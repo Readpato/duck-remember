2fc2f621dea6d6b7a1e4b08b203c20e9d01fb7ae
Patrick Raedler <code@readpato.dev>
2024-05-01 20:01:23 +0200

    refactor(CtaIcon): properly handle nuxtlink with as child

--- components/CtaIcon.vue
+++ components/CtaIcon.vue
@@ -2,7 +2,10 @@
 import type { ButtonVariants } from '~/components/ui/button'
 
 withDefaults(defineProps<{
+  asChild?: boolean
   icon: string
+  isAnchor?: boolean
+  to?: string
   variant?: ButtonVariants['variant']
 }>(), {
   variant: 'outline',
@@ -10,7 +13,14 @@ withDefaults(defineProps<{
 </script>
 
 <template>
-  <SCButton size="icon" :variant="variant" class="size-8">
-    <Icon :name="`carbon:${icon}`" class="size-5" />
+  <SCButton size="icon" :variant="variant" class="size-8" :as-child="asChild">
+    <template v-if="asChild && isAnchor">
+      <NuxtLink :to="to">
+        <Icon :name="`carbon:${icon}`" class="size-5" />
+      </NuxtLink>
+    </template>
+    <template v-else>
+      <Icon :name="`carbon:${icon}`" class="size-5" />
+    </template>
   </SCButton>
 </template>
