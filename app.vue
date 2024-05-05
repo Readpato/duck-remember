a4d9b136833ce9595cdde81c19782b585e99c464
Patrick Raedler <code@readpato.dev>
2024-05-05 19:39:59 +0200

    refactor(app): utilize NuxtLayout

--- app.vue
+++ app.vue
@@ -1,25 +1,5 @@
-<script setup lang="ts">
-const navbarEl = ref<HTMLDivElement | null>(null)
-const { height } = useElementBounding(navbarEl)
-</script>
-
 <template>
-  <div class="app bg-background">
-    <TheNavbar ref="navbarEl" />
-    <div class="content flex">
-      <TheSidebar :offset-height="height" />
-      <NuxtPage class="h-full overflow-y-auto" />
-    </div>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<style scoped lang="css">
-.app {
-  min-height: 100vh;
-  min-height: 100dvh;
-}
-
-.content {
-  height: v-bind('`calc(100dvh - ${height}px)`');
-}
-</style>
