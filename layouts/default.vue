a681ed201fca087c31bc2f50b6aa7ce6749f9bde
Patrick Raedler <code@readpato.dev>
2024-05-05 19:40:11 +0200

    feat(layouts): create basic layouts

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const navbarEl = ref<HTMLDivElement | null>(null)
+const { height } = useElementBounding(navbarEl)
+</script>
+
+<template>
+  <div class="app bg-background">
+    <TheNavbar ref="navbarEl" />
+    <div class="content flex">
+      <TheSidebar :offset-height="height" />
+      <NuxtPage class="h-full overflow-y-auto" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="css">
+.app {
+  min-height: 100vh;
+  min-height: 100dvh;
+}
+
+.content {
+  height: v-bind('`calc(100dvh - ${height}px)`');
+}
+</style>
---
+++ layouts/unauthenticated.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app grid grid-cols-1 md:grid-cols-2">
+    <div class="dark-mode:bg-background flex w-full flex-col items-center justify-center gap-y-2 bg-foreground">
+      <h1 class="dark-mode:text-foreground scroll-m-20 text-4xl font-extrabold tracking-tight text-background lg:text-5xl">
+        Remember
+      </h1>
+      <CtaIcon
+        is-anchor
+        as-child
+        icon="logo-github"
+        target="_blank"
+        to="https://www.github.com/readpato/duck-remember"
+        variant="outline"
+        class="dark-mode:text-foreground dark-mode:border-foreground border-background bg-foreground text-background"
+      />
+    </div>
+    <NuxtPage class="w-full" />
+  </div>
+</template>
+
+<style scoped lang="css">
+.app {
+  min-height: 100vh;
+  min-height: 100dvh;
+}
+</style>
