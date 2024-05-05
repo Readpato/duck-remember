1c8d7b95d96c4513f759939f8d60a5b6ec95c7f6
Patrick Raedler <code@readpato.dev>
2024-05-05 17:24:57 +0200

    refactor(pages): rename pages

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="container flex h-full flex-col py-4">
+    <h1 class="mb-2 text-xl font-bold">
+      Remember
+    </h1>
+  </main>
+</template>
---
+++ pages/write.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="container flex h-full flex-col py-4">
+    <h1 class="mb-2 text-xl font-bold">
+      Write
+    </h1>
+    <form class="flex h-full flex-col gap-2">
+      <SCTextarea placeholder="What would you like to remember in the future?" class="h-full resize-none" />
+      <SCButton class="w-fit self-end" size="sm">
+        Submit
+      </SCButton>
+    </form>
+  </main>
+</template>
