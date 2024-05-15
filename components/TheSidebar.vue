f7b832183c3b1571e4b64cc2be2b32ce54e646b9
Patrick Raedler <code@readpato.dev>
2024-05-15 17:40:13 +0200

    refactor(TheSidebar): structure buttons differently

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -12,7 +12,14 @@ const links = [{
 
 <template>
   <div class="h-full w-64 border-r p-4">
-    <SCButton v-for="{ text, route, key } in links" :key="key" as-child variant="outline" size="sm" class="mb-2 w-full last:mb-0">
+    <SCButton
+      v-for="{ text, route, key } in links"
+      :key="key"
+      as-child
+      variant="outline"
+      size="sm"
+      class="mb-2 w-full last:mb-0"
+    >
       <NuxtLink :to="route">
         {{ text }}
       </NuxtLink>
