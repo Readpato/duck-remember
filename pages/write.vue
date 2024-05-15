329bd423a729f09e338cee7d6d7941a0a99e06c4
Patrick Raedler <code@readpato.dev>
2024-05-15 17:24:13 +0200

    feat(pages/write): create placeholder for writing

--- pages/write.vue
+++ pages/write.vue
@@ -1,4 +1,22 @@
 <script setup lang="ts">
+import type { Database, PostgrestError } from '~/types'
+
+const supabase = useSupabaseClient<Database>()
+const entry = ref('')
+const hasError = ref<PostgrestError | null>(null)
+
+const handleSubmit = async () => {
+  const { error } = await supabase.from('entries').update({
+    data: entry.value,
+    id: 1,
+  })
+  return hasError.value = error
+}
+
+watch(hasError, (error) => {
+  if (error)
+    console.error(error)
+})
 </script>
 
 <template>
@@ -6,9 +24,9 @@
     <h1 class="mb-2 text-xl font-bold">
       Write
     </h1>
-    <form class="flex h-full flex-col gap-2">
-      <SCTextarea placeholder="What would you like to remember in the future?" class="h-full resize-none" />
-      <SCButton class="w-fit self-end" size="sm">
+    <form class="flex h-full flex-col gap-2" @submit.prevent="handleSubmit">
+      <SCTextarea v-model="entry" placeholder="What would you like to remember in the future?" class="h-full resize-none" />
+      <SCButton class="w-fit self-end" size="sm" type="submit">
         Submit
       </SCButton>
     </form>
