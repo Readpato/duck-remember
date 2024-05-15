71fbdb477055de395c5b081a5565b2a2c1a86904
Patrick Raedler <code@readpato.dev>
2024-05-15 17:22:22 +0200

    refactor(TheNavbar): add logout function

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+const supabase = useSupabaseClient()
 const colorMode = useColorMode()
+const hasError = ref()
 
 const colorModeIcon = computed(() => colorMode.preference === 'dark' ? 'sun' : 'moon')
 
@@ -7,6 +9,15 @@ const handleColorChange = () => {
   colorMode.preference === 'dark'
     ? colorMode.preference = 'light'
     : colorMode.preference = 'dark'
+}
+
+const handleSignOut = async () => {
+  const { error } = await supabase.auth.signOut()
+
+  if (error)
+    hasError.value = error
+
+  navigateTo({ path: '/login' })
 }
 </script>
 
@@ -18,6 +29,7 @@ const handleColorChange = () => {
     <div class="space-x-2">
       <CtaIcon is-anchor as-child icon="logo-github" target="_blank" to="https://www.github.com/readpato/duck-remember" />
       <CtaIcon :icon="colorModeIcon" @click="handleColorChange" />
+      <CtaIcon icon="logout" @click="handleSignOut" />
     </div>
   </header>
 </template>
