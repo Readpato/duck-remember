ff24a76b3235ddd1700237ae90bb1d11b102afaf
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-11-13 11:36:01 +0100

    feat: add popover and calendar components

--- pages/write.vue
+++ pages/write.vue
@@ -1,35 +1,38 @@
 <script setup lang="ts">
-import type { Database, PostgrestError } from '~/types'
+  import DatePicker from '~/components/ui/datepicker/DatePicker.vue'
+  import type { Database, PostgrestError } from '~/types'
 
-useHead({
-  title: 'Write',
-})
-
-const supabase = useSupabaseClient<Database>()
-const entry = ref('')
-const hasError = ref<PostgrestError | null>(null)
-
-const handleSubmit = async () => {
-  const { error } = await supabase.from('entries').update({
-    data: entry.value,
-    id: 1,
+  useHead({
+    title: 'Write',
   })
-  return hasError.value = error
-}
 
-watch(hasError, (error) => {
-  if (error)
-    console.error(error)
-})
+  const supabase = useSupabaseClient<Database>()
+  const entry = ref('')
+  const hasError = ref<PostgrestError | null>(null)
+
+  const handleSubmit = async () => {
+    const { error } = await supabase.from('entries').update({
+      data: entry.value,
+      id: 1,
+    })
+    return hasError.value = error
+  }
+
+  watch(hasError, (error) => {
+    if (error)
+      console.error(error)
+  })
 </script>
 
 <template>
   <main class="container flex h-full flex-col py-4">
+    <DatePicker />
     <h1 class="mb-2 text-xl font-bold">
       Write
     </h1>
     <form class="flex h-full flex-col gap-2" @submit.prevent="handleSubmit">
-      <SCTextarea v-model="entry" placeholder="What would you like to remember in the future?" class="h-full resize-none" />
+      <SCTextarea v-model="entry" placeholder="What would you like to remember in the future?"
+        class="h-full resize-none" />
       <SCButton class="w-fit self-end" size="sm" type="submit">
         Submit
       </SCButton>
